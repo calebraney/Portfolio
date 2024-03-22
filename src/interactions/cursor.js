7fc57d10dd99001e8779895435ed7d6f4d837faf
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2024-03-22 17:22:51 -0400

    actually fixed cursor

--- src/interactions/cursor.js
+++ src/interactions/cursor.js
@@ -1,82 +1,75 @@
 export const cursor = function () {
-  const cursorInner = document.querySelector('cursor="inner"');
-  const cursorOuter = document.querySelector('cursor="outer"');
+  const cursorInner = document.querySelector('[cursor="inner"]');
+  const cursorOuter = document.querySelector('[cursor="outer"]');
 
-  //CURSOR CODE
-  //Cursor .is-cursor-minor global
-  $(
-    '.is-cursor-minor, .text-style-link, .menu_small-text-link, .menu_link-medium, .nav-logo_component, .footer_primary-link, .footer_text-link, .footer_icon, .nav-button_component, .cta_block-item, .pitch_image-wrap, .home-work_text-link, .work_arrow-link'
-  ).on('mouseenter mouseleave', function () {
-    cursorInner.toggleClass('is-cursor-minor');
-    cursorOuter.toggleClass('is-cursor-minor');
-  });
+  // Option classes
+  const minorClass = 'is-cursor-minor';
+  const vanishedClass = 'is-vanished';
+  const viewPageClass = 'is-view-page';
+  const viewCaseClass = 'is-view-case';
+  const nextCaseClass = 'is-next-case';
+  const prevCaseClass = 'is-prev-case';
+  const letsGoClass = 'is-lets-go';
+  const imageBlackClass = 'is-image-black';
+  const imageWhiteClass = 'is-image-white';
+  const nextCaseWhiteClass = 'is-next-case-w';
+  const notBlendedClass = 'is-not-blended';
 
-  //Cursor .is-cursor-minor attribute
-  $('[cursor=minor]').on('mouseenter mouseleave', function () {
-    cursorInner.toggleClass('is-cursor-minor');
-    cursorOuter.toggleClass('is-cursor-minor');
-  });
+  // Function to toggle class
+  function toggleClass(element, className) {
+    if (element.classList.contains(className)) {
+      element.classList.remove(className);
+    } else {
+      element.classList.add(className);
+    }
+  }
 
-  //Cursor scrolls over item with view-page attribute
-  $('[cursor=view-page]').on('mouseenter mouseleave', function () {
-    console.log('view page');
-    cursorInner.toggleClass('is-vanished');
-    cursorOuter.toggleClass('is-view-page');
-    $('.cursor_component').toggleClass('is-not-blended');
-  });
+  // Function to handle mouseenter and mouseleave events
+  function handleMouseEvents(selector, classNames) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach((element) => {
+      element.addEventListener('mouseenter', () => {
+        classNames.forEach((className) => toggleClass(element, className));
+      });
+      element.addEventListener('mouseleave', () => {
+        classNames.forEach((className) => toggleClass(element, className));
+      });
+    });
+  }
 
-  //Cursor .is-view-case and .is-black
-  $('.home-work_item').on('mouseenter mouseleave', function () {
-    cursorInner.toggleClass('is-black');
-    cursorOuter.toggleClass('is-view-case');
-    $('.cursor_component').toggleClass('is-not-blended');
-  });
+  // Cursor .is-cursor-minor global
+  handleMouseEvents(
+    '.is-cursor-minor, .text-style-link, .menu_small-text-link, .menu_link-medium, .nav-logo_component, .footer_primary-link, .footer_text-link, .footer_icon, .nav-button_component, .cta_block-item, .pitch_image-wrap, .home-work_text-link, .work_arrow-link',
+    [minorClass]
+  );
 
-  //Cursor .is-view-case and .is-vanished
-  $('.work_button-circle').on('mouseenter mouseleave', function () {
-    cursorInner.toggleClass('is-vanished');
-    cursorOuter.toggleClass('is-view-case');
-    $('.cursor_component').toggleClass('is-not-blended');
-  });
+  // Cursor .is-cursor-minor attribute
+  handleMouseEvents('[cursor=minor]', [minorClass]);
 
-  //Cursor .is-next-case global
-  $('.is-next-case').on('mouseenter mouseleave', function () {
-    cursorInner.toggleClass('is-vanished');
-    cursorOuter.toggleClass('is-next-case');
-    $('.cursor_component').toggleClass('is-not-blended');
-  });
+  // Cursor scrolls over item with view-page attribute
+  handleMouseEvents('[cursor=view-page]', [vanishedClass, viewPageClass, notBlendedClass]);
 
-  //Cursor .is-prev-case global
-  $('.is-prev-case').on('mouseenter mouseleave', function () {
-    cursorInner.toggleClass('is-vanished');
-    cursorOuter.toggleClass('is-prev-case');
-    $('.cursor_component').toggleClass('is-not-blended');
-  });
+  // Cursor .is-view-case and .is-black
+  handleMouseEvents('.home-work_item', ['is-black', viewCaseClass, notBlendedClass]);
 
-  //Cursor scrolls over item with lets-go attribute
-  $('[cursor=lets-go]').on('mouseenter mouseleave', function () {
-    cursorInner.toggleClass('is-vanished');
-    cursorOuter.toggleClass('is-lets-go');
-    $('.cursor_component').toggleClass('is-not-blended');
-  });
+  // Cursor .is-view-case and .is-vanished
+  handleMouseEvents('.work_button-circle', [vanishedClass, viewCaseClass, notBlendedClass]);
 
-  //Cursor scrolls over item with image-black attribute
-  $('[cursor=image-black]').on('mouseenter mouseleave', function () {
-    cursorInner.toggleClass('is-image-black');
-    cursorOuter.toggleClass('is-cursor-minor');
-    $('.cursor_component').toggleClass('is-not-blended');
-  });
+  // Cursor .is-next-case global
+  handleMouseEvents('.is-next-case', [vanishedClass, nextCaseClass, notBlendedClass]);
 
-  //Cursor scrolls over item with image-white attribute
-  $('[cursor=image-white]').on('mouseenter mouseleave', function () {
-    cursorInner.toggleClass('is-image-white');
-    cursorOuter.toggleClass('is-cursor-minor');
-    $('.cursor_component').toggleClass('is-not-blended');
-  });
+  // Cursor .is-prev-case global
+  handleMouseEvents('.is-prev-case', [vanishedClass, prevCaseClass, notBlendedClass]);
 
-  //Cursor is next case white on case study pages
-  $('.cs-next_component').on('mouseenter mouseleave', function () {
-    cursorOuter.toggleClass('is-next-case-w');
-    $('.cursor_component').toggleClass('is-not-blended');
-  });
+  // Cursor scrolls over item with lets-go attribute
+  handleMouseEvents('[cursor=lets-go]', [vanishedClass, letsGoClass, notBlendedClass]);
+
+  // Cursor scrolls over item with image-black attribute
+  handleMouseEvents('[cursor=image-black]', [imageBlackClass, minorClass, notBlendedClass]);
+
+  // Cursor scrolls over item with image-white attribute
+  handleMouseEvents('[cursor=image-white]', [imageWhiteClass, minorClass, notBlendedClass]);
+
+  // Cursor is next case white on case study pages
+  handleMouseEvents('.cs-next_component', [nextCaseWhiteClass, notBlendedClass]);
 };
